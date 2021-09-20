e23ed475e2149c6721becbafdac3b8c7477dc9f2
rmsepulveda <rmsepulveda@sbcglobal.net>
2021-09-19 18:34:03 -0700

    J

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,8 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 
+
+
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
