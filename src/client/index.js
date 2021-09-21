34104ff4f25a6f027b9a956bc5bdcd3210333cac
rmsepulveda <rmsepulveda@sbcglobal.net>
2021-09-20 17:53:47 -0700

    s

--- src/client/index.js
+++ src/client/index.js
@@ -1,11 +1,17 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
+// sass files
+import './styles/resets.css'
+import './styles/base.css'
+import './styles/footer.css'
+import './styles/form.css'
+import './styles/header.css'
 
 
 console.log(checkForName);
 
-alert("I EXIST")
+//alert("I EXIST")
 console.log("CHANGE!!");
 
 
