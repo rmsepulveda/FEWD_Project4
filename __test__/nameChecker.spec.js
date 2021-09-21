9850c2592888e5333e2dcb82f575aac248ed52b4
rmsepulveda <rmsepulveda@sbcglobal.net>
2021-09-21 11:01:04 -0700

    d

---
+++ __test__/nameChecker.spec.js
@@ -0,0 +1,6 @@
+import { checkForName } from "../src/client/js/nameChecker"
+
+describe("Testing the submit functionality", () => {
+    test("Testing the checkIfURL() function", () => {
+        expect(checkForName).toBeDefined();
+})});
